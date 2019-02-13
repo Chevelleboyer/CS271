5a60ccc2589ccf3e97b2b93d53db655fa94b59ee
Chevelle Boyer <chevelleboyer@gmail.com>
2019-02-13 10:22:44 -0800

    Implemented Solution to I/O Handling Program.

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -11,26 +11,59 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
+(READKEY)	
+	@KBD
+	D=M
+	@RESETB
+	D;JNE
+	@RESETW
+	D;JEQ
+
+(RESETW)	
 	@SCREEN
 	D=A
 	@i
 	M=D
-//(LOOP)
-	//@KBD
-	//D=M
-	//@LOOP
-	//D;JEQ
-(LOOP)
+	@WHITE
+	0;JMP
+
+(RESETB)
+	@SCREEN
+	D=A
+	@i
+	M=D
+	@BLACK
+	0;JMP
+
+(WHITE)
 	@i
 	D=M
 	@KBD
 	D=D-A
-	@END
-	D;JGT
+	@READKEY
+	D;JGE
+	@i
+	A=M
+	M=0
+	@i
+	M=M+1
+	@WHITE
+	0;JMP
+
+(BLACK)
+	@i
+	D=M
+	@KBD
+	D=D-A
+	@READKEY
+	D;JGE
 	@i
 	A=M
 	M=-1
 	@i
 	M=M+1
-	@LOOP
+	@BLACK
 	0;JMP
+
+
+
